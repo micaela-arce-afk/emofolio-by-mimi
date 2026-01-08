a413e01a00050212daf9f1e65fdd26787ae21202
Micaela <micaela.arce.b@gmail.com>
2026-01-08 22:14:20 +0100

    8

--- portfolio-template/assets/js/main.js
+++ portfolio-template/assets/js/main.js
@@ -6,30 +6,15 @@ darkBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 });
 
-// Carrusel automático + botones
+// Carrusel automático
 const carouselItems = document.querySelectorAll(".carousel-item");
-const prevBtn = document.querySelector(".carousel-btn.prev");
-const nextBtn = document.querySelector(".carousel-btn.next");
 let currentIndex = 0;
 
-// Función mostrar siguiente
 function showNext() {
   carouselItems[currentIndex].classList.remove("active");
   currentIndex = (currentIndex + 1) % carouselItems.length;
   carouselItems[currentIndex].classList.add("active");
 }
 
-// Función mostrar anterior
-function showPrev() {
-  carouselItems[currentIndex].classList.remove("active");
-  currentIndex =
-    (currentIndex - 1 + carouselItems.length) % carouselItems.length;
-  carouselItems[currentIndex].classList.add("active");
-}
-
 // Cambio automático cada 3 segundos
 setInterval(showNext, 3000);
-
-// Botones Next / Prev
-nextBtn.addEventListener("click", showNext);
-prevBtn.addEventListener("click", showPrev);
